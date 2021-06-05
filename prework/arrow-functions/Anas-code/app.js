7cf28cbf974b238caf58b6d355fbdcd355268d9f
anas <civil.anas1000@gmail.com>
2021-06-05 05:14:48 +0300

    the first one

--- prework/arrow-functions/Anas-code/app.js
+++ prework/arrow-functions/Anas-code/app.js
@@ -114,7 +114,11 @@ let sum = function(a, b, c, d) {
 };
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(sum(1, 2, 3, 4));
+console.log(sum(1, 2, 3, 4));
+
+
+let newSum = (a,b,c,d) => `${a+b+c+d}`
+console.log(`the New Sum is ${newSum(1,2,3,4)}`);
 
 
 let objectLit = function() {
@@ -126,7 +130,7 @@ let objectLit = function() {
 };
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(objectLit());
+console.log(objectLit());
 
 
 let sumAndProduct = function(a, b) {
